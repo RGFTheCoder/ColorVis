b6619167b1d005c55a7aaf2e8028ede1315bab83
RGFTheCoder <recdeclec.ceo@gmail.com>
2018-05-12 18:16:28 -0400

    upd2

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var hex = searchParams.get('h');
 onload = function() {
     if (hex != null) {
         clr = "#" + hex;
-        
     }
     document.querySelector("#text").style.background = clr;
     document.querySelector("#text").textContent = clr;
